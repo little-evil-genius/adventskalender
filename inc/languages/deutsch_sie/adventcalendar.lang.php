7e66e05a3179c52c1ea77981a0cd9d7afcd8b54a
Lara <75126217+little-evil-genius@users.noreply.github.com>
2022-02-27 22:50:58 +0100

    Create adventcalendar.lang.php

---
+++ inc/languages/deutsch_sie/adventcalendar.lang.php
@@ -0,0 +1,11 @@
+<?php
+$l['adventcalendar_name'] = "Adventskalender";
+$l['adventcalendar_door'] = "Adventskalender - Tür";
+$l['adventcalendar_door_nav'] = "Adventskalender - Tür {1}";
+
+$l['adventcalendar_deactivate'] = "<div style=\"text-align:center;margin:10px auto;\">Das Team hat den Adventskalender aktuell deaktiviert!</div>";
+$l['adventcalendar_notday'] = "<div style=\"text-align:center;margin:10px auto;\">Haben wir heute etwa schon den {1}.12?!<br>NEEEE, heute ist erst der <b>{2}.12</b>! Also Schummeln ist nicht!<br><a href=\"adventskalender.php\">Zurück zum Adventskalender</a></div>";
+$l['adventcalendar_nottext'] = "<div style=\"text-align:center;margin:10px auto;\">Huch, da hat das Team wohl den Inhalt vergessen! Da waren die Wichtel nicht fleißig.<br><a href=\"adventskalender.php\">Zurück zum Adventskalender</a></div>";
+
+$l['adventcalendar_redirect_uninstall'] = "Der Adventskalender ist nicht installiert! Du wirst nun auf dem Index zurückgeleitet."
+?>
