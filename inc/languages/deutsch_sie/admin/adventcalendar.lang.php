4280874863a88e5c314e6a36a75f8f37e6ef2aa1
Lara <75126217+little-evil-genius@users.noreply.github.com>
2022-02-27 22:50:41 +0100

    Create adventcalendar.lang.php

---
+++ inc/languages/deutsch_sie/admin/adventcalendar.lang.php
@@ -0,0 +1,38 @@
+<?php
+// Plugin Beschreibung und Name
+$l['adventcalendar_name'] = "Adventskalender";
+$l['adventcalendar_short_desc'] = "Mit diesem Plugin lassen sich über das AdminCP die Inhalte für den Adventskalender (sämtliche Formatierungsmöglichkeiten erlaubt) erstellen. Diese werden anschließend unter url.de/adventskalender.php aufgelistet.";
+// Admin Rechte
+$l['adventcalendar_permission'] = "Kann den Inhalt des Adventskalender verwalten?";
+// Adventskalender Seite
+$l['adventcalendar_manage'] = "Adventskalender verwalten";
+$l['adventcalendar_manage_overview_entries'] = "Adventskalender Tage";
+$l['adventcalendar_manage_overview_entries_desc'] = "Hier findest du eine Übersicht der Tage und deren Inhalte vom Adventskalender.";
+$l['adventcalendar_manage_add_entry'] = "Hinzufügen";
+$l['adventcalendar_manage_add_entry_desc'] = "Hier fügst du einen neuen Tag und den Inhalt für diesen Tag hinzu.";
+$l['adventcalendar_manage_edit_entry'] = "Tag bearbeiten";
+$l['adventcalendar_manage_edit_entry_desc'] = "Hier bearbeitst du einen bestehenden Tag und seinen Inhalt.";
+$l['adventcalendar_manage_days'] = "Kalendertag";
+$l['adventcalendar_manage_title'] = "Kurzinformation Inhalt";
+$l['adventcalendar_manage_options'] = "Optionen";
+// Optionsbutton
+$l['adventcalendar_manage_edit'] = "Bearbeiten";
+$l['adventcalendar_manage_delete'] = "Löschen";
+$l['adventcalendar_manage_error_invalid'] = "Ungültige Option";
+// Hinzufügen und Bearbeiten
+$l['adventcalendar_manage_error_no_day'] = "Kein Tag angegeben.";
+$l['adventcalendar_manage_error_no_title'] = "Kein Title angegeben.";
+$l['adventcalendar_manage_error_no_text'] = "Kein Inhalt angegeben.";
+$l['adventcalendar_manage_entry_day_title'] = "Tag";
+$l['adventcalendar_manage_entry_day_desc'] = "Für welchen Tag soll dieser Inhalt angezeigt werden?";
+$l['adventcalendar_manage_entry_title_title'] = "Kurzinformation Inhalt";
+$l['adventcalendar_manage_entry_title_desc'] = "Wie kann man den Inhalt von diesem Tag knapp beschreiben? Ein Beispiel wäre: Keksrezept. Dieses Schlagwort soll dem Team bei der Orientierung helfen.";
+$l['adventcalendar_manage_entry_text_title'] = "Inhalt";
+$l['adventcalendar_manage_entry_text_desc'] = "Was versteckt sich hinter diesem Türchen? Beachte, dass es eine Zeichenbegrenzung von <b>500.000 Zeichen</b> gibt!";
+$l['adventcalendar_manage_submit_add'] = "Tag hinzufügen";
+$l['adventcalendar_manage_submit_edit'] = "Tag bearbeiten";
+
+$l['adventcalendar_manage_entry_added'] = "Der neue Tag und Inhalt wurde hinzugefügt.";
+$l['adventcalendar_manage_entry_edited'] = "Der Tag und dessen Inhalt wurden bearbeitet.";
+$l['adventcalendar_manage_entry_deleted'] = "Der Tag und sein Inhalt wurden gelöscht.";
+?>
